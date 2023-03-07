1ee524cdb6df7366fee3dc402994ae917a9b0511
Ivan20092887 <ivan.tarasov.spb@gmail.com>
2023-03-07 19:01:06 +0300

    Move to GitHub server

--- 07 10001st prime.py
+++ 07 10001st prime.py
@@ -1,2 +1,14 @@
-number = 13
-count = 6
+number= 10001
+result = 3
+count = 1
+prime = 2
+
+while count < number:
+    for x in range(3, int(result ** 0.5) + 1, 2):
+        if result % x == 0:
+            break
+    else:
+        prime = result
+        count += 1
+    result += 2
+print(prime)
